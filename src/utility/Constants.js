cc927123e2c827612767b4f6db79513c15cdf3da
realPeso <Someoneatstowe@gmail.com>
2019-08-03 01:27:31 -0500

    Update Constants.js

--- src/utility/Constants.js
+++ src/utility/Constants.js
@@ -401,7 +401,7 @@ class Constants {
         maxChar: 24,
         cooldownRaid: 28800000,
         cooldownWithdraw: 14400000,
-        creationCost: -1,
+        creationCost: 100000000,
         nameChange: 500,
         raidOdds: 80
       },
